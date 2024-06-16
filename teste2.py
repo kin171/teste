36cd1773ca36e13988876df7c13570a516302f35
kin171 <kin5090@hotmail.com>
2024-06-16 13:19:11 -0300

    pul

--- teste2.py
+++ teste2.py
@@ -1,6 +1,12 @@
+nome=input('Qual o seu nome ?')
+es=input('Qual o estado que vc nasceu?')
 t1=input('Que ano vç nasceu?')
 from datetime import date
+from datetime import datetime
 t2=date.today().year
+t3=datetime.now().time()
+t3format=t3.strftime('%H:%M:%S')
 calc=int(t2) - int(t1)
-print('Vc tem ',calc, 'anos')
-print ('eu sou um viadao')
+print(nome,'Vc tem ',calc, 'anos e nasceu no estado ',es)
+print ('vc esta no ano de',t2,)
+print(t3format)
