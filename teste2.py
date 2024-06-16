29cc73aeadc140463af11e04a88e9623d2f5418a
kin171 <kin5090@hotmail.com>
2024-06-16 17:26:29 -0300

    a

--- teste2.py
+++ teste2.py
@@ -1,14 +1,16 @@
-nome=input('Qual o seu nome ?')
-an=input('Qual seu anime esta vendo agora?')
-fi=input('qual seu filme favorito')
-es=input('Qual o estado que vc nasceu?')
-t1=input('Que ano vç nasceu?')
 from datetime import date
 from datetime import datetime
-t2=date.today().year
-t3=datetime.now().time()
-t3format=t3.strftime('%H:%M:%S')
-calc=int(t2) - int(t1)
-print(nome,'Vc tem ',calc, 'anos \n nasceu no estado ',es)
-print ('vc esta no ano de',t2,)
-print(t3format)
+
+nome=input('Qual o seu nome ?')
+anime=input('Qual seu anime esta vendo agora?')
+filme=input('qual seu filme favorito')
+estado=input('Qual o estado que vc nasceu?')
+anoNascimento=input('Que ano vç nasceu?')
+
+ano=date.today().year
+hora=datetime.now().time()
+horaFormat=hora.strftime('%H:%M:%S')
+calcularIdade=int(ano) - int(anoNascimento)
+print('Seja bem vindo ',nome,'. \nVc tem ',calcularIdade, ' anos \n nasceu no estado: ',estado, '\n esta vendo o anime',anime, '\n Seu filme favorito é',filme)
+print ('vc esta no ano de',ano,)
+print(horaFormat)
