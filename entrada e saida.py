159604d0bab80df562e364393ceddc257d9255c6
kin171 <kin5090@hotmail.com>
2024-06-16 19:51:00 -0300

    S

---
+++ entrada e saida.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+entrada=input datetime('horario de entrada?')
+saida=input datetime('horario de saida?')
+turno=int(saida) -int(entrada) 
--- teste2.py
+++ teste2.py
@@ -5,12 +5,17 @@ nome=input('Qual o seu nome ?')
 anime=input('Qual seu anime esta vendo agora?')
 filme=input('qual seu filme favorito')
 estado=input('Qual o estado que vc nasceu?')
+serie=input('Que serie esta vendo no momento?')
+mes=input('QUAL MES VC NASCEU?')
+DIA=INPUT('QUAL DIA VC NASCEU?')
 anoNascimento=input('Que ano vç nasceu?')
-
 ano=date.today().year
+MESAGORA=DATE.TODAY().MONTH
+DIAAGORA=DATE.TODAY().DAY
 hora=datetime.now().time()
 horaFormat=hora.strftime('%H:%M:%S')
 calcularIdade=int(ano) - int(anoNascimento)
+
 print('Seja bem vindo ',nome,'. \nVc tem ',calcularIdade, ' anos \n nasceu no estado: ',estado, '\n esta vendo o anime',anime, '\n Seu filme favorito é',filme)
 print ('vc esta no ano de',ano,)
 print(horaFormat)
