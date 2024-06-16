ddbefdd47d0e7c48c7b1b903b09fc3834ac98188
kin171 <kin5090@hotmail.com>
2024-06-16 17:12:41 -0300

    as

--- teste2.py
+++ teste2.py
@@ -1,4 +1,6 @@
 nome=input('Qual o seu nome ?')
+an=input('Qual seu anime esta vendo agora?')
+fi=input('qual seu filme favorito')
 es=input('Qual o estado que vc nasceu?')
 t1=input('Que ano vç nasceu?')
 from datetime import date
@@ -7,6 +9,6 @@ t2=date.today().year
 t3=datetime.now().time()
 t3format=t3.strftime('%H:%M:%S')
 calc=int(t2) - int(t1)
-print(nome,'Vc tem ',calc, 'anos e nasceu no estado ',es)
+print(nome,'Vc tem ',calc, 'anos \n nasceu no estado ',es)
 print ('vc esta no ano de',t2,)
 print(t3format)
