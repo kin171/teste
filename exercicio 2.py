67a42f1ac40053e5b311d933e4ebdfa5a5abdb91
kin171 <kin5090@hotmail.com>
2024-06-17 18:29:55 -0300

    sim

--- exercicio 2.py
+++ exercicio 2.py
@@ -1,4 +1,12 @@
-n1=input('digite um numero')
-n2=input('digite outro numero ')
-s=int(n1) * int(n2)
-print('o valor e',s)
+from datetime import datetime, timedelta
+
+dataInicial=input('data inicial?')
+datafinal=input('data final?')
+n1=float(input('digite um valor:'))
+n2=float(input('digite um valor :'))
+sub=datetime(datafinal)-datetime(dataInicial)
+s= (n1 + n2)
+print('o valor de {} '.format (s))
+print('A soma de ',n1, 'mais',n2, 'é igual a',s)
+print('A soma entre {} {} é igual {}'.format(n1, n2, s))
+print('a subtração das horas é',sub)
