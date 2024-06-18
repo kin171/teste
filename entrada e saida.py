5479dd9c1cd85cc59b6093298905c2a8640b1ef8
kin171 <kin5090@hotmail.com>
2024-06-18 19:33:27 -0300

    k

--- entrada e saida.py
+++ entrada e saida.py
@@ -1,4 +1,9 @@
-from datetime import datetime
-entrada=input datetime('horario de entrada?')
-saida=input datetime('horario de saida?')
-turno=int(saida) -int(entrada) 
+import datetime
+
+data1 = datetime.datetime(2023, 3, 8)  # Altere para a primeira data
+data2 = datetime.datetime(2023, 3, 1)  # Altere para a segunda data
+
+diferenca = data1 - data2
+
+print("Diferença em dias:", diferenca.days)
+
