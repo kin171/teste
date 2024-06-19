9578ce385bdc8a0c713325f6a7dd2aaa6e8d0ffb
kin171 <kin5090@hotmail.com>
2024-06-18 23:17:29 -0300

    s

--- entrada e saida.py
+++ entrada e saida.py
@@ -1,5 +1,8 @@
 from datetime import datetime, timedelta
 
+
+duracao = timedelta(hours = 1)  # Soma 2 horas
+
 dataentrada = input('Data entrada:')
 datasaida = input('Data saida:')
 horaalmoco=('1:00')
@@ -11,16 +14,17 @@ horaAlmoco = datetime.strptime(horaalmoco,('%H:%M'))
 data=(1900, 1, 1)
 diferenca = datas - datae
 dif1 = datas - horaAlmoco
-dif2 = datae - horaAlmoco 
-dif3 = dif1 - dif2
+dif2 = datae + duracao 
+dif3 = datae - dif2
 
-total = ( dif3 - dif2)
+
+#total = ( dif3 - dif2)
 
 print("Diferença em dias:", diferenca)
-print(f'A jornada foi de {total}')
+#print(f'A jornada foi de {total}')
 print(diferenca)
 print(dif1)
 print(dif2)
 print(dif3)
 print(data)
-print(total)
+#print(total)
