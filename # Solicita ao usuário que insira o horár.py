f07da7162126c6a8165edbc7c9abee54efa73766
Antonio Honorio <kin5090@hotmail.com>
2024-06-19 10:41:12 -0300

    a

--- # Solicita ao usuário que insira o horár.py
+++ # Solicita ao usuário que insira o horár.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from os import write
 
 
 duracao = timedelta(hours = 1)  # Soma 1 horas
@@ -14,6 +15,28 @@ diferenca = datas - datae
 #dif1 = datas - horaAlmoco
 dif2 = diferenca - duracao 
 #dif3 = datae - dif2
+open ('jornada agricola.txt', 'a')
+
+dados = [datae, datas, diferenca]
+def salvar_informacoes(dados, jornada agricola):
+ # """
+ # Função para salvar dados em um arquivo de texto.
+
+  #Args:
+     # dados: Uma lista de dados a serem salvos.
+     # nome_arquivo: O nome do arquivo para salvar os dados.
+  #"""
+  with open('jornada agricola.txt', "a") as arquivo:
+    for dado in dados:
+      arquivo.write(dado + "\n")
+
+# Exemplo de uso
+#dados = ["Dado 1", "Dado 2", "Dado 3"]
+salvar_informacoes(dados, "jornada agricola.txt")
+
+#for dado in dados:
+    #arquivo.write(dado + '\n')
+jornada agricola.close()
 
 
 #total = ( dif3 - dif2)
