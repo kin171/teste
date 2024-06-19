05039ae920469708d7d5372857e50e3eb37f1893
kin171 <kin5090@hotmail.com>
2024-06-19 20:55:56 -0300

    r

--- # Solicita ao usuário que insira o horár.py
+++ # Solicita ao usuário que insira o horár.py
@@ -5,10 +5,10 @@ duracao = timedelta(hours = 1)  # Soma 1 horas
 
 dataentrada = input('Data entrada e hora:')
 datasaida = input('Data saida e hora:')
-horaalmoco=('1:00')
+#horaalmoco=('1:00')
 datae = datetime.strptime(dataentrada,('%d/%m/%Y %H:%M'))
 datas = datetime.strptime(datasaida,('%d/%m/%Y %H:%M'))
-horaAlmoco = datetime.strptime(horaalmoco,('%d/%m/%Y %H:%M'))
+#horaAlmoco = datetime.strptime(horaalmoco,('%d/%m/%Y %H:%M'))
 
 diferenca = datas - datae
 #dif1 = datas - horaAlmoco
@@ -20,24 +20,33 @@ dif2 = diferenca - duracao
 #arquivo = open('jornada agricola.txt', 'a')
 #def salvar_informacoes(dados, arquivo):
 
-  #with open('jornada agricola.txt', 'a') as arquivo:
-    #for linha in dados:
-     # arquivo.write(linha + '\n')
-
-
+#  with open('jornada agricola.txt', 'a') as arquivo:
+#    for linha in dados:
+ #     arquivo.write(linha + '\n')
+import json
 dados = [datae, datas, diferenca]
-with open('jornada agricola.txt', 'a') as arquivo:
-  arquivo.write(dados)
+def salvar_dados(dados, jornada agricola):
+    with open(jornada agricola, "a") as arquivo:
+        json.dump(dados, arquivo)
+
+# Example usage
+#dados = {"nome": "Fulano de Tal", "idade": 30, "cidade": "São Paulo"}
+salvar_dados(dados, "jornada agricola.json")
 
 
-#salvar_informacoes(dados, "jornada agricola,txt")
+
+#with open('jornada agricola.txt', 'a') as arquivo:
+ # arquivo.write(dados)
+
+
+salvar_informacoes(dados, "jornada agricola,txt")
 
 #arquivo.close()
 
 
 #total = ( dif3 - dif2)
 #print("Diferença em dias:", dif2)
-print('A jornada foi de {}'.format(dif2))
+#print('A jornada foi de {}'.format(dif2))
 #print(diferenca)
 #print(dif1)
 #print(dif2)
