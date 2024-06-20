2cba07169cac5fb404d16c7023e4988369528425
kin171 <kin5090@hotmail.com>
2024-06-20 17:39:41 -0300

    t

--- entradasaida.py
+++ entradasaida.py
@@ -10,41 +10,23 @@ duracao = timedelta(hours = 1)  # Soma 1 horas
 dataentrada = input('Data entrada e hora:')
 
 datasaida = input('Data saida e hora:')
-
+horaEntrada = '7:20'
 
 datae = datetime.strptime(dataentrada,('%d/%m/%Y %H:%M'))
 
 datas = datetime.strptime(datasaida,('%d/%m/%Y %H:%M'))
-
+dif1 = datetime.strptime(horaEntrada,('%d/%m/%Y %H:%M'))
 
 
 diferenca = datas - datae
 dif2 = diferenca - duracao 
-dados = [datae, datas, dif2]
-#def salvar_informacoes(dados, arquivo):
-
-#    with open('jornada-agricola.txt', 'a') as arquivo:
-
- #       for linha in dados:
-
- #           arquivo.write(linha +'\n')
-#salvar_informacoes(dados, 'jornada-agricola.txt')
-
-
-# Definindo a variável "dados"
-dados = [datae, datas, dif2]  # Substitua com seus valores reais
-
-# Abrindo o arquivo para escrita (modo "a" para anexar ao final)
-
-def salvar(dados, arquivo):
-    with open('jornada_agricola.txt', 'a') as arquivo:
-
-    # Convertendo os dados para JSON (opcional, mas recomendado para melhor formatação)
-        dados_json = json.dumps(dados)
-
+if dif2 >= dif1:
+    dif3 = datas - dif1
+dado =str(datae)+ ' ;'+ str(datas)+ ';'+ str(dif2) + ';'+ str(dif3)  # Substitua com seus valores reais
+with open('jornada_agricola.txt', 'a') as arquivo:
     # Escrevendo os dados no arquivo
-        arquivo.write(dados_json + '\n')  # Adiciona quebra de linha no final
+    arquivo.write(dado + '\n')  # Adiciona quebra de linha no final
 
-salvar(dados, 'jornada_agricola.txt')
+
 
 
