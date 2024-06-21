eb732d7c55e8098bc3da3aa864b1e555e7cfce1c
Antonio Honorio <kin5090@hotmail.com>
2024-06-21 08:36:03 -0300

    sj

--- entradasaida.py
+++ entradasaida.py
@@ -1,21 +1,4 @@
 from datetime import datetime, timedelta
-<<<<<<< HEAD
-import os
-import json
-duracao = timedelta(hours = 1)  # Soma 1 horas
-dataentrada = input('Data entrada e hora:')
-datasaida = input('Data saida e hora:')
-datae = datetime.strptime(dataentrada,('%d/%m/%Y %H:%M'))
-datas = datetime.strptime(datasaida,('%d/%m/%Y %H:%M'))
-diferenca = datas - datae
-dif2 = diferenca - duracao 
-dados = [datae, datas, dif2]
-def salvar(dados, arquivo):
-    with open('jornada_agricola.txt', 'a') as arquivo:
-        dados_json = json.dumps(dados)
-        arquivo.write(dados_json + '\n')  # Adiciona quebra de linha no final    
-        salvar(dados, 'jornada_agricola.txt')
-=======
 
 duracao = timedelta(hours = 1)  # Soma 1 horas
 dataentrada = input('Data entrada e hora:')
@@ -35,8 +18,3 @@ dado =str(datae) + ';'+ str(datas)+ ';' + str(jornada) + ';'+ str(dif3)  # Subst
 with open('jornada_agricola.txt', 'a') as arquivo:
     # Escrevendo os dados no arquivo
     arquivo.write(dado + '\n')  # Adiciona quebra de linha no final
-
-
->>>>>>> 9722029137d0a25e17c84ad8dd67c1ee779200ae
-
-
