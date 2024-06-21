52d9a9ba6ca8cfdb03ffa0d256eb70c52c9274b3
Antonio Honorio <kin5090@hotmail.com>
2024-06-21 14:21:29 -0300

    docs: Atualizações de funções para melhor legibilidade e reutilização de codigo

--- entradasaida.py
+++ entradasaida.py
@@ -80,16 +80,17 @@ while  encerrar == 'N':
     horaextra = calcularHoraExtra(jornada)
 #____________________________________
 #Valores Formatados_____________________
-valor = formatarValoresString(frota, turno, datae, datas, jornada, horaextra)
-    
-    #valores = str(frota) + ';' + str(turno) + ';' + str(datae) + ';'+ str(datas)+ ';' + str(jornada) + ';'+ str(horaextra)
+    valores = formatarValoresString(frota, turno, datae, datas, jornada, horaextra)
     print(valores)
+#_______________________________________
 
     confirma = input("\n Dados corretos?(s/n) ")
-    confirma = confirma.upper
-
+    confirma = confirma.upper()
+    
     while confirma == "N":
         if confirma == "N":
+
+            #TODO Corrigir validação de caso
             corrige = input("\n Qual dado quer alterar?\n 1 para Frota \n 2 para Turno \n 3 para Data e Hora Entrada \n 4 para Data e Hora Saida \n ")
             if corrige == 1:
                 frota = definirFrota()
@@ -105,18 +106,16 @@ valor = formatarValoresString(frota, turno, datae, datas, jornada, horaextra)
                 break
             else:
                 break
-        
-        if jornada >= horaNormal:
-            horaextra = jornada - horaNormal
-        else:
-            horaextra = 0
 
-        valores = str(frota) + ';' + str(turno) + ';' + str(datae) + ';'+ str(datas)+ ';' + str(jornada) + ';'+ str(horaextra)
+        jornada = calcularJornada(datae, datas)
+        horaextra = calcularHoraExtra(jornada)
+        
+        valores = formatarValoresString(frota, turno, datae, datas, jornada, horaextra)
         print(valores)
 
         confirma = input("\n Dados corretos?(s/n) ")
-        confirma = confirma.upper
+        confirma = confirma.upper()
 
     encerrar = input('Deseja encerrar o programa? (s/n): ')
-    encerrar = encerrar.upper
+    encerrar = encerrar.upper()
     print ("Volte sempre!!")
