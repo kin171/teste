9722029137d0a25e17c84ad8dd67c1ee779200ae
Antonio Honorio <kin5090@hotmail.com>
2024-06-21 07:57:25 -0300

    d

--- entradasaida.py
+++ entradasaida.py
@@ -9,12 +9,12 @@ datas = datetime.strptime(datasaida,('%d/%m/%Y %H:%M'))
 
 horaNormal = timedelta(hours=7, minutes=20)
 diferenca = datas - datae
-dif2 = diferenca - duracao 
-print(dif2)
-if dif2 >= horaNormal:
-    dif3 = dif2 - horaNormal
-    
-dado =str(datae)+ ' ;'+ str(datas)+ ';'+ str(dif2) + ';'+ str(dif3)  # Substitua com seus valores reais
+jornada = diferenca - duracao 
+print(jornada)
+if jornada >= horaNormal:
+    dif3 = jornada - horaNormal
+
+dado =str(datae) + ';'+ str(datas)+ ';' + str(jornada) + ';'+ str(dif3)  # Substitua com seus valores reais
 with open('jornada_agricola.txt', 'a') as arquivo:
     # Escrevendo os dados no arquivo
     arquivo.write(dado + '\n')  # Adiciona quebra de linha no final
