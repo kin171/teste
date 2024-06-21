4182b650cca1c96bcc250dc4b2a4ee9c86c48f2a
Antonio Honorio <kin5090@hotmail.com>
2024-06-21 07:26:46 -0300

    s

--- entradasaida.py
+++ entradasaida.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-import os
+
 duracao = timedelta(hours = 1)  # Soma 1 horas
 dataentrada = input('Data entrada e hora:')
 datasaida = input('Data saida e hora:')
@@ -7,14 +7,15 @@ horanormal = ('7:20')
 datae = datetime.strptime(dataentrada,('%d/%m/%Y %H:%M'))
 datas = datetime.strptime(datasaida,('%d/%m/%Y %H:%M'))
 dif1 = datetime.strptime(horanormal,('%H:%M')).time()
-hj = datetime.today()
-combined_datetime = datetime(year=hj.year, month=hj.month, day=hj.day,
-                                     hour=dif1.hour, minute=dif1.minute, second=0)
+#hj = datetime.today()
+#combined_datetime = datetime(year=hj.year, month=hj.month, day=hj.day,
+hour=dif1.hour, minute=dif1.minute, second=0)
 
+horaNormal = timedelta(hours=7, minutes=20)
 diferenca = datas - datae
 dif2 = diferenca - duracao 
 print(dif2)
-if dif2 >= combined_datetime:
+if dif2 >= horaNormal:
     dif3 = datas - combined_datetime
 dado =str(datae)+ ' ;'+ str(datas)+ ';'+ str(dif2) + ';'+ str(dif3)  # Substitua com seus valores reais
 with open('jornada_agricola.txt', 'a') as arquivo:
