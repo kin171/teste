581865c9ea411244fc18332d0eb387fe0402d6f0
kin171 <kin5090@hotmail.com>
2024-06-25 08:13:37 -0300

    p

--- extrair data.py
+++ extrair data.py
@@ -4,11 +4,11 @@ data = input("Digite a data no formato dd/mm/aaaa: ")
 dats = datetime.strptime(data, "%d/%m/%Y") 
 print (dats)
 datahj = datetime.today()
-datae = datetime.strptime(datahj,'%d/%m/%Y')
+datae = datetime.strftime(datahj,'%d/%m/%Y')
 print (datae)
 
 
-if dats == datae:
+if data == datae:
     print("A data informada é hoje!")
 else:
     print("A data informada não é hoje.")
