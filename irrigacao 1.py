dc5b898b4fe79b28701b36bdf8126b9d3c4629c5
kin171 <kin5090@hotmail.com>
2024-07-15 04:57:57 -0300

    R

--- irrigacao 1.py
+++ irrigacao 1.py
@@ -5,7 +5,7 @@ import csv
 lista_codop = ['1 Irrigação','2 Fertirrigação','3 Aplic bioestimulante','4 Aplic inset','5 Aplic nematicida','6 Aplicação vinhaça','7 Lavagem de arrasto','8 Lavagem sistema','9 Limpeza em blocos','10 Limpeza rede principal','11 Montagem de válvulas','12 Chuva','13 Dry Off (preparação  dry off)','14 Enchimento reservatório','15 Falta de energia','16 Manutenção elétrica','17 Manutenção hidraulica (malha)','18 Manutenção mecanica','19 Solo úmido','99 sem irrigação']
 lista_projetos = ['P1','P2','P3','P4','P5','P6','P7','P8','P9','P10','P11','P12','P13','P14','P15','P16']
 lista_setor = ['S1','S2','S3','S4','S5']
-lista_irrigação = []
+
 
 janela = tk.Tk()
 
@@ -18,53 +18,55 @@ def inserir_codigo():
     setor = combobox_selecionar_tipo1.get()
     operacao = combobox_selecionar_tipo2.get()
     insumo = entry_insumo.get()
-    insumo2 = entry_insumo1.get()
+    insumo1 = entry_insumo1.get()
+    insumo2 = entry_insumo2.get()
     insumo3 = entry_insumo3.get()
     insumo4 = entry_insumo4.get()
     insumo5 = entry_insumo5.get()
     insumo6 = entry_insumo6.get()
     insumo7 = entry_insumo7.get()
-    insumo8 = entry_insumo8.get()
     hora_inicial = entry_horainicial.get()
     #hora_inicial = hora_inicial.strftime('%H:%M')
     hora_final = entry_horafinal.get()
     #hora_final = hora_final.strftime('%H:%M')
+    #grava os arquivos
+    with open('baseirrigacao.csv','a') as arquivo:
+        writer = csv.writer(arquivo)
+        writer.writerow([data, projeto, setor, operacao, insumo, insumo1, insumo2, insumo3, insumo4, insumo5, insumo6, insumo7, hora_inicial, hora_final])
     
-    lista_irrigação.append((data, projeto, setor, operacao, insumo, insumo2, insumo3, insumo4, insumo5, insumo6, insumo7, insumo8, hora_inicial, hora_final))
-    with open('baseirrigacao.csv', 'a') as arquivo:
-        linha = csv.writer(arquivo)
-        linha.writerow(lista_irrigação)
+    #limpa os campos do formulario
     entry_data.delete(0, tk.END)
     combobox_selecionar_tipo.delete(0, tk.END)
     combobox_selecionar_tipo1.delete(0, tk.END)
     combobox_selecionar_tipo2.delete(0, tk.END)
     entry_insumo.delete(0, tk.END)
-    entry_insumo3.delete(0, tk.END)
+    entry_insumo1.delete(0, tk.END)
+    entry_insumo2.delete(0, tk.END)
     entry_insumo3.delete(0, tk.END)
     entry_insumo4.delete(0, tk.END)
     entry_insumo5.delete(0, tk.END)
     entry_insumo6.delete(0, tk.END)
     entry_insumo7.delete(0, tk.END)
-    entry_insumo8.delete(0, tk.END)
     entry_horainicial.delete(0, tk.END)
     entry_horafinal.delete(0, tk.END)
-        
+#botao de limpar        
 def limpar():
     entry_data.delete(0, tk.END)
     combobox_selecionar_tipo.delete(0, tk.END)
     combobox_selecionar_tipo1.delete(0, tk.END)
     combobox_selecionar_tipo2.delete(0, tk.END)
     entry_insumo.delete(0, tk.END)
-    entry_insumo3.delete(0, tk.END)
+    entry_insumo1.delete(0, tk.END)
+    entry_insumo2.delete(0, tk.END)
     entry_insumo3.delete(0, tk.END)
     entry_insumo4.delete(0, tk.END)
     entry_insumo5.delete(0, tk.END)
     entry_insumo6.delete(0, tk.END)
     entry_insumo7.delete(0, tk.END)
-    entry_insumo8.delete(0, tk.END)
     entry_horainicial.delete(0, tk.END)
     entry_horafinal.delete(0, tk.END)
     return 
+#botao fechar
 def fechar():
     janela.destroy()
 
@@ -74,14 +76,11 @@ def fechar():
 #Título da Janela
 
 janela.title('INSERÇÃO DE BASE DE DADOS')
-
+#botoes e campos
 label_descricao = tk.Label(text="BASE DE DADOS")
 label_descricao.grid(row=1, column=0,padx = 10, pady=10, sticky='nswe', columnspan =4 )
 
-#entry_descricao = tk.Entry()
 
-#entry_descricao1 = tk.label(text='projeto')
-#entry_descricao.grid(row=2,column=0, padx=10, pady=10, sticky='nswe', columnspan = 2)
 label_data =  tk.Label(text="DATA")
 label_data.grid(row=2, column=0,padx = 10, pady=10, sticky='nswe', columnspan =2 )
 entry_data =  tk.Entry()
@@ -114,35 +113,35 @@ label_insumo1.grid(row=7, column=0,padx = 10, pady=10, sticky='nswe', columnspan
 entry_insumo1 =  tk.Entry()
 entry_insumo1.grid(row=7, column=2,padx = 10, pady=10, sticky='nswe', columnspan =2 )
 
-label_insumo3 =  tk.Label(text="INSUMO 2")
-label_insumo3.grid(row=8, column=0,padx = 10, pady=10, sticky='nswe', columnspan =2 )
+label_insumo2 =  tk.Label(text="INSUMO 2")
+label_insumo2.grid(row=8, column=0,padx = 10, pady=10, sticky='nswe', columnspan =2 )
+entry_insumo2 =  tk.Entry()
+entry_insumo2.grid(row=8, column=2,padx = 10, pady=10, sticky='nswe', columnspan =2 )
+
+label_insumo3 =  tk.Label(text="INSUMO 3")
+label_insumo3.grid(row=9, column=0,padx = 10, pady=10, sticky='nswe', columnspan =2 )
 entry_insumo3 =  tk.Entry()
-entry_insumo3.grid(row=8, column=2,padx = 10, pady=10, sticky='nswe', columnspan =2 )
+entry_insumo3.grid(row=9, column=2,padx = 10, pady=10, sticky='nswe', columnspan =2 )
 
-label_insumo4 =  tk.Label(text="INSUMO 3")
-label_insumo4.grid(row=9, column=0,padx = 10, pady=10, sticky='nswe', columnspan =2 )
+label_insumo4 =  tk.Label(text="INSUMO 4")
+label_insumo4.grid(row=10, column=0,padx = 10, pady=10, sticky='nswe', columnspan =2 )
 entry_insumo4 =  tk.Entry()
-entry_insumo4.grid(row=9, column=2,padx = 10, pady=10, sticky='nswe', columnspan =2 )
+entry_insumo4.grid(row=10, column=2,padx = 10, pady=10, sticky='nswe', columnspan =2 )
 
-label_insumo5 =  tk.Label(text="INSUMO 4")
-label_insumo5.grid(row=10, column=0,padx = 10, pady=10, sticky='nswe', columnspan =2 )
+label_insumo5 =  tk.Label(text="INSUMO 5")
+label_insumo5.grid(row=11, column=0,padx = 10, pady=10, sticky='nswe', columnspan =2 )
 entry_insumo5 =  tk.Entry()
-entry_insumo5.grid(row=10, column=2,padx = 10, pady=10, sticky='nswe', columnspan =2 )
+entry_insumo5.grid(row=11, column=2,padx = 10, pady=10, sticky='nswe', columnspan =2 )
 
-label_insumo6 =  tk.Label(text="INSUMO 5")
-label_insumo6.grid(row=11, column=0,padx = 10, pady=10, sticky='nswe', columnspan =2 )
+label_insumo6 =  tk.Label(text="INSUMO 6")
+label_insumo6.grid(row=12, column=0,padx = 10, pady=10, sticky='nswe', columnspan =2 )
 entry_insumo6 =  tk.Entry()
-entry_insumo6.grid(row=11, column=2,padx = 10, pady=10, sticky='nswe', columnspan =2 )
+entry_insumo6.grid(row=12, column=2,padx = 10, pady=10, sticky='nswe', columnspan =2 )
 
-label_insumo7 =  tk.Label(text="INSUMO 6")
-label_insumo7.grid(row=12, column=0,padx = 10, pady=10, sticky='nswe', columnspan =2 )
+label_insumo7 =  tk.Label(text="INSUMO 7")
+label_insumo7.grid(row=13, column=0,padx = 10, pady=10, sticky='nswe', columnspan =2 )
 entry_insumo7 =  tk.Entry()
-entry_insumo7.grid(row=12, column=2,padx = 10, pady=10, sticky='nswe', columnspan =2 )
-
-label_insumo8 =  tk.Label(text="INSUMO 7")
-label_insumo8.grid(row=13, column=0,padx = 10, pady=10, sticky='nswe', columnspan =2 )
-entry_insumo8 =  tk.Entry()
-entry_insumo8.grid(row=13, column=2,padx = 10, pady=10, sticky='nswe', columnspan =2 )
+entry_insumo7.grid(row=13, column=2,padx = 10, pady=10, sticky='nswe', columnspan =2 )
 
 
 label_horainicial =  tk.Label(text="HORA INICIAL")
